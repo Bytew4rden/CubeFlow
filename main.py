169a907e306a0d6e751b7874983220f0c6e98179
Peyton Frehner <pfrehner98@gmail.com>
2026-03-02 13:57:44 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+
+app = Flask(__name__)
+solves = []
+@app.route("/",methods=['GET','POST'])
+def postInfo():
+    
+    if request.method == 'POST':
+        data = request.get_json()
+
+        solve={
+            "solveNumber":data["solveNumber"],
+            "scramble":data["scramble"],
+            "time":data["time"]
+        }
+
+        solves.append( str(solve["solveNumber"]) + ": " + solve["scramble"] + " | " + str(solve["time"]) + "s" )
+    return render_template('index.html',solves=solves)
+
+if __name__ == '__main__':
+    app.run(debug=True)
