3a114db032f6ad69406de1c56d63b82b316dfd0d
Peyton Frehner <pfrehner98@gmail.com>
2026-03-04 22:08:00 -0800

    can make an account, and we check for duplicates

--- main.py
+++ main.py
@@ -2,15 +2,31 @@ from flask import Flask,render_template,request,redirect,url_for
 import sqlite3
 
 app = Flask(__name__)
+database = 'cubeflow.db'
 
-database = sqlite3.connect('site.db')
+createTablesQuery = """
+CREATE TABLE IF NOT EXISTS users
+(
+user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT UNIQUE NOT NULL,
+password TEXT NOT NULL
+);
 
-cursor = database.cursor()
+CREATE TABLE IF NOT EXISTS solves
+(
+solve_id INTEGER PRIMARY KEY AUTOINCREMENT,
+time_seconds REAL NOT NULL,
+scramble TEXT,
+user_id INTEGER NOT NULL,
+FOREIGN KEY (user_id) REFERENCES users(user_id) 
+);
+"""
+conn = sqlite3.connect(database)
+cur = conn.cursor()
+cur.execute(createTablesQuery)
+conn.close()
 
-command1="""CREATE TABLE IF NOT EXISTS
-Users(user_id INTEGER PRIMARY KEY, )"""
-
-# Routes #
+# Auth "Blueprint" Routes #
 @app.route("/",methods=["GET","POST"])
 def home():
     print("Connected user not logged in, redirecting to login page.")
@@ -20,13 +36,28 @@ def home():
 def login():
     return render_template('login.html')
 
-@app.route("/createacct",methods=["GET","POST"])
-def createacct():
+@app.route("/register",methods=["GET","POST"])
+def register():
     if request.method=="POST":
-        print('',end='')
+        username = request.form.get('un')
+        password = request.form.get('pw')
+
+        query = 'SELECT username FROM users WHERE username = ?'
+        conn=sqlite3.connect(database)
+        cur=conn.cursor()
+        cur.execute(query,(username,))
+        result = cur.fetchone()
+        
+        if result:
+            conn.close()
+            return render_template('register.html')
+        else:
+            query = f'INSERT INTO users(username,password) VALUES ({username},{password})'
+            cur.execute(query)
+            conn.close()
+            return 
     else:
-        print("rendering account creation page")
-        return render_template('createaccount.html')
+        return render_template('register.html')
 
 
 if __name__ == '__main__':
